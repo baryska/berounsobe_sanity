727947e6cf22cbffbfd655d373865bb0ed08b9f7
baryska <barbora.skalova@newtontech.cz>
2024-11-19 21:15:17 +0100

    add schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,8 +4,9 @@ import imageContent from './imageContent';
 import postContent from './postContent';
 import imageGroup from './imageGroup';
 import faqContent from './faqContent';
+import timelineContent from './timelineContent';
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [textContent, imageContent, postContent, imageGroup, faqContent],
+  types: [textContent, imageContent, postContent, imageGroup, faqContent, timelineContent],
 }
---
+++ schemaTypes/timelineContent.ts
@@ -0,0 +1,9 @@
+import { SchemaTypeDefinition } from 'sanity';
+
+const timelineContent: SchemaTypeDefinition = {
+  name: 'timelineContent',
+  title: 'Timeline Content',
+  type: 'text',
+};
+
+export default timelineContent;
