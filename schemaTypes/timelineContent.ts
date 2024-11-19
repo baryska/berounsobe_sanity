4e236d7b1c0ceabf471628a3bedc887f7a9c889f
baryska <barbora.skalova@newtontech.cz>
2024-11-19 21:21:27 +0100

    adjust schema

--- schemaTypes/timelineContent.ts
+++ schemaTypes/timelineContent.ts
@@ -3,7 +3,10 @@ import { SchemaTypeDefinition } from 'sanity';
 const timelineContent: SchemaTypeDefinition = {
   name: 'timelineContent',
   title: 'Timeline Content',
-  type: 'text',
+  type: 'document',
+  fields: [
+    { name: 'text', title: 'Text', type: 'string' },
+  ]
 };
 
 export default timelineContent;
