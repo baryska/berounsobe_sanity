fa1f57b41c9bb88f95db567de77c4633a6997abe
baryska <barbora.skalova@newtontech.cz>
2025-03-23 11:09:17 +0100

    adjust post schema

--- schemaTypes/postContent.ts
+++ schemaTypes/postContent.ts
@@ -8,12 +8,50 @@ const postContent: SchemaTypeDefinition = {
     { name: 'title', title: 'Title', type: 'string' },
     { name: 'theme', title: 'Theme', type: 'string' },
     { name: 'key', title: 'Key', type: 'number' },
-    { name: 'slug', title: 'Slug', type: 'slug'},
+    { name: 'slug', title: 'Slug', type: 'slug' },
     { name: 'date', title: 'Date', type: 'string' },
     { name: 'image', title: 'Image', type: 'image' },
-    { name: 'additionalImages', title: 'Additional Images', type: 'array', of: [{ type: 'image' }] },
-    { name: 'paragraphs', title: 'Paragraphs', type: 'array', of: [{ type: 'block' }] },
+    {
+      name: 'additionalImages',
+      title: 'Additional Images',
+      type: 'array',
+      of: [{ type: 'image' }],
+    },
+    {
+      name: 'paragraphs',
+      title: 'Paragraphs',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+            annotations: [
+              {
+                name: 'link',
+                type: 'object',
+                title: 'Odkaz',
+                fields: [
+                  {
+                    name: 'href',
+                    type: 'url',
+                    title: 'URL',
+                    validation: (Rule) => Rule.uri({
+                      scheme: ['http', 'https', 'mailto', 'tel']
+                    })
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    },
   ],
 };
 
 export default postContent;
+
